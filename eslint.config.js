9b9c056820d632029e1274209c47862fa23dd566
Wriddhi Hazra <wriddhihazra@gmail.com>
2024-08-28 21:23:44 +0530

    bootstrapped astro with react and set up tailwind and eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.astro'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unknown-property': 'off',
+    },
+  },
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.astro'],
+    rules: {
+      // override/add rules settings here, such as:
+      'astro/no-set-html-directive': 'error',
+    },
+  },
+];
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React, { type FC } from 'react';
+
+const Footer: FC<{ author: string }> = ({ author }) => {
+  return <div>Made with ❤️ by {author}</div>;
+};
+
+export default Footer;
