3cdda272538e52245d1db899f3aa2b46b9976b3b
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-09-09 08:55:19 +0000

    feat: add lan swicher to header on mobiles

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,7 +2,6 @@ import { getRelativeLocaleUrl } from 'astro:i18n';
 import { BsGithub } from 'react-icons/bs';
 import { GoHomeFill } from 'react-icons/go';
 import { MdTranslate } from 'react-icons/md';
-import { GiHamburgerMenu } from 'react-icons/gi';
 import {
   cn,
   getAlternateLanguage,
@@ -42,7 +41,7 @@ function generateNavItems(path: string, currentLang: 'en' | 'bn'): Array<NavItem
     {
       label: oppositeLang === 'en' ? 'English' : 'বাংলা',
       href: getAlternateLanguagePath(path),
-      icon: <MdTranslate className="size-6 fill-primary-foreground" />,
+      icon: <MdTranslate className="size-4 lg:size-6 fill-primary-foreground" />,
     },
   ];
 }
@@ -72,33 +71,19 @@ export const LargeNavbar = ({ path }: Props) => {
   );
 };
 
-export const MobileHamburgerNavbar = ({ path }: Props) => {
+export const MobileHeaderLangSwitcher = ({ path }: Props) => {
   const currentLang = getCurrentLanguage(path);
   const navItems = generateNavItems(path, currentLang);
 
   return (
-    <>
-      <button className="outline-none mobile-menu-button mt-5">
-        <GiHamburgerMenu className="h-5 w-5" />
-      </button>
-
-      <div className="hidden">
-        {navItems.map((item, index) => (
-          <a
-            key={index}
-            href={item.href}
-            data-active={(item.href === path || item.href === '/bn') && index === 0} // hotfix for netlify hidden trailing slash issue
-            className={cn(
-              'outline outline-primary-foreground outline-1 bg-primary-background',
-              'data-[active=true]:outline-2 data-[active=true]:bg-[#fed7aa]',
-              'p-2 grid place-items-center rounded-lg w-16',
-            )}
-          >
-            {item.icon}
-            <span className="font-work text-xs font-semibold">{item.label}</span>
-          </a>
-        ))}
-      </div>
-    </>
+    <a
+      href={navItems[2].href}
+      className={cn(
+        'outline outline-primary-foreground outline-1 bg-primary-background',
+        'flex items-center justify-center rounded-md p-2 w-8 h-8',
+      )}
+    >
+      {navItems[2].icon}
+    </a>
   );
 };
