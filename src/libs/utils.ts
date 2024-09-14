2147557292e36ca511412f37a970307af687ccf2
Wriddhi Hazra <wriddhihazra@gmail.com>
2024-09-14 23:32:03 +0530

    chore: setup react-query with nanostores

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -26,3 +26,51 @@ export const getAlternateLanguagePath = (currentPath: string): string => {
   const newPath = currentPath === '/' ? '/bn' : `/bn${currentPath}`;
   return newPath;
 };
+
+// utility object to convert time units to number
+export const time = {
+  milliseconds: (value: number) => value,
+  seconds: (value: number) => value * 1000,
+  minutes: (value: number) => value * 1000 * 60,
+  hours: (value: number) => value * 1000 * 60 * 60,
+  days: (value: number) => value * 1000 * 60 * 60 * 24,
+  weeks: (value: number) => value * 1000 * 60 * 60 * 24 * 7,
+  months: (value: number) => value * 1000 * 60 * 60 * 24 * 30,
+  years: (value: number) => value * 1000 * 60 * 60 * 24 * 365,
+} as const;
+
+type StorageData<Data> = {
+  value: Data;
+  expiry: number | null;
+};
+
+export const cacheStore = {
+  get: <Data>(key: string): Data | null => {
+    const data = localStorage.getItem(key);
+    if (!data) {
+      return null;
+    }
+    const { value, expiry } = JSON.parse(data) as StorageData<Data>;
+
+    const hasExpired = expiry !== null && Date.now() > expiry;
+
+    if (hasExpired) {
+      localStorage.removeItem(key);
+      return null;
+    }
+    return value;
+  },
+  set: (key: string, value: unknown, expiry: number) => {
+    const data: StorageData<unknown> = {
+      value,
+      expiry: Date.now() + expiry,
+    };
+    localStorage.setItem(key, JSON.stringify(data));
+  },
+  delete: (key: string) => {
+    localStorage.removeItem(key);
+  },
+  deleteAll: () => {
+    localStorage.clear();
+  },
+} as const;
