9635b86aa2ec8133edd79132904ba55c30b5ae64
Wriddhi Hazra <wriddhihazra@gmail.com>
2024-09-16 21:44:11 +0530

    chore: added https to dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'bn'],
