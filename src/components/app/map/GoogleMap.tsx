67549e1ae489d0e59281cbc33694b449d7523785
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-09-18 13:05:51 +0530

    chore: cleanup user location

--- src/components/app/map/GoogleMap.tsx
+++ src/components/app/map/GoogleMap.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useMemo, useState } from 'react';
-import { APIProvider, Map, type MapCameraChangedEvent } from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 import { UserLocation } from './UserLocation';
 import { DEFAULT_VIEW_LAT_LONG } from '@/libs/config';
 import type { GoogleMapProps } from './types';
@@ -17,16 +17,13 @@ export const GoogleMaps = ({ apiKey, pandals }: GoogleMapProps) => {
 
   return (
     <section className="max-w-screen h-[500px] overflow-hidden rounded-[22px] focus:outline-none">
-      <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
+      <APIProvider apiKey={apiKey}>
         <Map
           id="map"
           gestureHandling={'greedy'}
           defaultZoom={zoom}
           defaultCenter={center}
           mapId={'4e06f8f1228c0ba9'}
-          onCameraChanged={(ev: MapCameraChangedEvent) =>
-            console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-          }
           onClick={handleMapClick}
           className="relative w-full h-full"
         >
