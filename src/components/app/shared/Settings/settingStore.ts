e52261be3b4f06d03e372f14d65b76179c0c92c5
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2024-09-22 20:58:57 +0530

    fix: removed unused files and made client:only-react

--- src/components/app/shared/Settings/settingStore.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from 'nanostores';
-
-export const isSettingsOpen = atom(false);
--- src/components/app/shared/Settings/settingsToggle.tsx
+++
@@ -1,8 +0,0 @@
-import { useStore } from '@nanostores/react';
-import { isSettingsOpen } from './settingStore';
-
-export default function SettingsBtn() {
-  const $isSettingOpen = useStore(isSettingsOpen);
-
-  return <button onClick={() => isSettingsOpen.set(!$isSettingOpen)}>Setting</button>;
-}
