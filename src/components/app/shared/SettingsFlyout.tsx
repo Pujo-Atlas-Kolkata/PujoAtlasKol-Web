a9baa38cda58efb2fa41d4d60077b62cdaa92ca9
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-09-23 14:07:37 +0530

    fix: settings ui

--- src/components/app/shared/SettingsFlyout.tsx
+++ src/components/app/shared/SettingsFlyout.tsx
@@ -1,5 +1,6 @@
 import { Drawer } from 'vaul';
 import { useState, useEffect } from 'react';
+import { Socials } from '@/constants';
 
 export default function SettingsFlyout() {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,39 +25,61 @@ export default function SettingsFlyout() {
             <Drawer.Portal>
               <Drawer.Overlay className="inset-0 bg-opacity-70 z-20 m-2" />
               <Drawer.Content
-                className="fixed bottom-0 left-0 right-0 max-h-[40%] 
-             bg-[#eae5d9] p-3
+                className="drop-shadow-md border border-black fixed bottom-0 left-0 right-0 max-h-[40%] 
+             bg-[#eae5d9] p-1
              z-30 rounded-3xl
              mx-4
              mb-16
              max-w-[calc(100% - 1rem)]
              shadow-lg"
               >
-                <Drawer.Title className="text-lg font-semibold">
-                  <div className="flex justify-between items-center space-x-1">
-                    <span className="sm:text-xl">Language</span>
-                    <div className="flex items-center bg-[#ebe5d9] rounded-r-full p-1 w-fit">
-                      <button className="bg-[#c5b394] text-white px-4 py-2 rounded-l-full transition-all font-bold">
+                <Drawer.Title className="text-base font-semibold px-0 py-2">
+                  <div className="flex flex-row justify-between items-center px-3">
+                    <div className="text-xl font-semibold">Language</div>
+                    <div className="flex items-center bg-[#ebe5d9] rounded-r-full p-1 w-fit text-base">
+                      <button className="bg-[#c5b394] text-white px-3 py-2 rounded-l-full transition-all font-bold border-l-2 border-t-2 border-b-2 border-black">
                         English
                       </button>
 
                       <button
                         disabled
-                        className="text-black px-2 py-1 font-bold relative flex flex-col justify-evenly"
+                        className="text-black px-5 py-1 font-bold relative flex flex-col justify-center items-center rounded-r-full border-2 border-black"
                       >
-                        <span className="absolute">বাংলা</span>
-                        <span className="text-[8px] text-gray-300 pt-7">Coming Soon</span>
+                        <span className="font-noto text-lg pt-1 line-through decoration-4">
+                          বাংলা
+                        </span>
                       </button>
                     </div>
                   </div>
+                  <div className="pt-2 pr-2 text-right font-sans text-xs">
+                    <span className="font-noto font-bold">বাংলা</span>&nbsp;language support coming
+                    soon!
+                  </div>
                 </Drawer.Title>
-                <hr className="border-t-2 border-gray-300" />
 
-                <div className="">
-                  <p className="text-lg font-semibold">Location Access</p>
-                  <p className="text-xs pt-1">
+                <hr className="border-gray-950/10 border-[0.5px] mx-3" />
+
+                <div className="flex flex-col items-start justify-center gap-y-0 px-3 py-1.5">
+                  <div className="text-lg font-semibold">Location Access</div>
+                  <div className="text-xs">
                     This can be managed in the browser per-site settings
-                  </p>
+                  </div>
+                </div>
+
+                <hr className="border-gray-950/10 border-[0.5px] mx-3" />
+
+                <div className="flex flex-row justify-center items-center text-xs py-2 mx-auto">
+                  <span className="text-left">
+                    Made with ❤️ by&nbsp;
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href={Socials.GitHub}
+                      className="font-semibold"
+                    >
+                      Pujo Atlas Kolkata
+                    </a>
+                  </span>
                 </div>
               </Drawer.Content>
             </Drawer.Portal>
