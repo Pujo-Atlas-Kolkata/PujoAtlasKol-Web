de197de51095ac3586c1b8561f01354f8fde598f
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-09-24 20:24:35 +0530

    fix: settings UI

--- src/components/app/shared/SettingsFlyout.tsx
+++ src/components/app/shared/SettingsFlyout.tsx
@@ -26,7 +26,7 @@ export default function SettingsFlyout() {
               <Drawer.Overlay className="inset-0 bg-opacity-70 z-20 m-2" />
               <Drawer.Content
                 className="drop-shadow-md top-[calc(100%-11.5rem)] fixed left-0 right-0 max-h-[40%] 
-             bg-[#e6dfcf] p-1
+             bg-[#dddddd]/90 p-1
              z-30 rounded-3xl
              mx-0
              mb-2
@@ -35,10 +35,10 @@ export default function SettingsFlyout() {
              shadow-lg"
               >
                 <Drawer.Title className="text-base px-0 py-0">
-                  <div className="rounded-full h-[6px] bg-[#c5b494] w-9 mb-3 mt-1 mx-auto"></div>
+                  <div className="rounded-full h-[6px] bg-[#8f7952] w-9 mb-3 mt-1 mx-auto"></div>
                   <div className="flex flex-row justify-between items-center px-3 py-0">
                     <div className="text-lg">Language</div>
-                    <div className="flex items-center bg-[#e6dfcf] rounded-r-full p-1 w-fit text-base">
+                    <div className="flex items-center bg-inherit rounded-r-full p-1 w-fit text-base">
                       <button className="bg-[#c5b394] font-semibold text-white px-3 py-2 text-sm rounded-l-full transition-all">
                         English
                       </button>
