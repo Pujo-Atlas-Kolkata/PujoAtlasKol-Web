6497fa82980a68b893c4a2e338321a57c2162841
Wriddhi Hazra <wriddhihazra@gmail.com>
2024-10-01 03:01:51 +0530

    feat: added teams data

---
+++ src/constants/team.ts
@@ -0,0 +1,208 @@
+export const Tech = ['Web', 'Mobile', 'Backend', 'DevOps', 'Data Science'] as const;
+
+export const NonTech = ['Design', 'Content', 'Coordinator', 'Community'] as const;
+
+type MemberDetails = {
+  id: string;
+  avatar: string;
+  name: string;
+} & (
+  | {
+      department: (typeof Tech)[number];
+      socials: {
+        discord: string;
+        github: string;
+        linkedin?: string;
+        twitter?: string;
+      };
+    }
+  | {
+      department: (typeof NonTech)[number];
+      socials: {
+        discord: string;
+        github?: string;
+        linkedin?: string;
+        twitter?: string;
+      };
+    }
+);
+
+export const team: Record<string, MemberDetails[]> = {
+  Founders: [
+    {
+      id: 'leofossilis',
+      name: 'leofossilis',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=fossils',
+      socials: {
+        discord: 'https://discord.com/users/1082731230916788336',
+      },
+      department: 'Content',
+    },
+  ],
+  Leads: [
+    {
+      id: 'rabbitz',
+      name: 'rabbitz',
+      avatar:
+        'https://api.dicebear.com/9.x/lorelei/svg?glassesProbability=100&seed=rabbitz&glasses=variant04',
+      socials: {
+        discord: 'https://discord.com/users/1033076736973361192',
+        linkedin: 'https://www.linkedin.com/in/ayanava-karmakar',
+        github: 'https://github.com/AyanavaKarmakar',
+      },
+      department: 'Web',
+    },
+    {
+      id: 'nox',
+      name: 'nox',
+      avatar:
+        'https://api.dicebear.com/9.x/lorelei/svg?glassesProbability=100&seed=thor&glasses=variant01',
+      socials: {
+        discord: 'https://discord.com/users/464333825137180672',
+        github: 'https://github.com/wriddhi',
+        linkedin: 'https://www.linkedin.com/in/wriddhi-hazra',
+      },
+      department: 'Web',
+    },
+    {
+      id: 'heisenberg',
+      name: 'Heisenberg',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=heisenberg',
+      socials: {
+        discord: 'https://discord.com/users/758731279625093138',
+        github: 'https://github.com/Heisen47',
+      },
+      department: 'Web',
+    },
+    {
+      id: 'inceptor',
+      name: 'Sbasu2512',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=inceptor&eyes=variant18',
+      socials: {
+        discord: 'https://discord.com/users/798395088593354762',
+        linkedin: 'https://www.linkedin.com/in/sayantan-basu-73ab4a92/',
+        github: 'https://github.com/Sbasu2512',
+      },
+      department: 'Backend',
+    },
+    {
+      id: 'akshay',
+      name: 'Akshay A Parmar',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=aksha',
+      socials: {
+        discord: 'https://discord.com/users/892804617128251452',
+        github: 'https://github.com/vitaminncpp',
+      },
+      department: 'Backend',
+    },
+    {
+      id: 'thehappybaloney',
+      name: 'TheHappyBaloney',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=baloon',
+      socials: {
+        discord: 'https://discord.com/users/1232913986689962078',
+        github: 'https://github.com/TheHappyBaloney',
+        twitter: 'https://x.com/TheHappyBaloney',
+        linkedin: 'https://www.linkedin.com/in/thehappybaloney',
+      },
+      department: 'Mobile',
+    },
+    {
+      id: 'babumosai',
+      name: 'Babumosai',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=babumosai',
+      socials: {
+        discord: 'https://discord.com/users/1265190463456022550',
+        github: 'https://github.com/thebabumoshai',
+      },
+      department: 'DevOps',
+    },
+    {
+      id: 'Chiken naget',
+      name: 'Chiken naget',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=naget',
+      socials: {
+        discord: 'https://discord.com/users/1209132404501913620',
+      },
+      department: 'Design',
+    },
+    {
+      id: 'WhiteTiger',
+      name: 'WhiteTiger',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=tiger',
+      socials: {
+        discord: 'https://discord.com/users/751361976369807370',
+      },
+      department: 'Community',
+    },
+    {
+      id: 'rishi',
+      name: 'rishi',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=rishi',
+      socials: {
+        discord: 'https://discord.com/users/283499137977024513',
+        github: 'https://github.com/saptarshichakrabarti',
+      },
+      department: 'Coordinator',
+    },
+  ],
+  'Web Team': [
+    {
+      id: 'aizaysi',
+      name: 'aizaysi',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=serbi',
+      socials: {
+        discord: 'https://discord.com/users/1224812991262884033',
+        github: 'https://github.com/aizaysi',
+      },
+      department: 'Web',
+    },
+    {
+      id: 'ritesh',
+      name: 'Ritesh Das',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=john',
+      socials: {
+        discord: 'https://discord.com/users/1257600086331101260',
+        github: 'https://github.com/Dyslex7c',
+        linkedin: 'https://www.linkedin.com/in/ritesh-das-066205288/',
+      },
+      department: 'Web',
+    },
+    {
+      id: 'yash',
+      name: 'Yash Chaurasia',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=yash',
+      socials: {
+        discord: 'https://discord.com/users/615100553122414592',
+        github: 'https://github.com/yashchaurasia667',
+        linkedin: 'https://www.linkedin.com/in/yash-chaurasia-259553196/',
+      },
+      department: 'Web',
+    },
+  ],
+  'Backend Team': [
+    {
+      id: 'indranil',
+      name: 'Indranil Chakraborty ',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=indranil',
+      socials: {
+        discord: 'https://discord.com/users/629236605936205835',
+        linkedin: 'https://www.linkedin.com/in/indranil-chakraborty-91ba60217',
+        github: 'https://github.com/Andrew99xx',
+      },
+      department: 'Backend',
+    },
+  ],
+  'Mobile Team': [
+    {
+      id: 'divyakumar',
+      name: 'DivyaKumar Patel ',
+      avatar: 'https://api.dicebear.com/9.x/lorelei/svg?seed=divyakum',
+      socials: {
+        discord: 'https://discord.com/users/1057312782401421332',
+        github: 'https://github.com/Divyakumar21202',
+      },
+      department: 'Mobile',
+    },
+  ],
+};
