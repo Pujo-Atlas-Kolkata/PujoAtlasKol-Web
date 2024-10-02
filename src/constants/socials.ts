5b3d7ecb4f38dd278b21c4ecc21bbde0fce78530
Wriddhi Hazra <wriddhihazra@gmail.com>
2024-10-03 00:27:56 +0530

    feat: updated socials

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -2,4 +2,7 @@ export const Socials = {
   GitHub: 'https://github.com/Pujo-Atlas-Kolkata/PujoAtlasKol-Web',
   Discord: 'https://discord.com/invite/xxSXWYf6d4',
   Reddit: 'https://www.reddit.com/r/kolkata',
+  Instagram: 'https://www.instagram.com/pujoatlas/',
+  Twitter: 'https://x.com/PujoAtlas',
+  Facebook: 'https://www.facebook.com/profile.php?id=61566663481562',
 } as const;
