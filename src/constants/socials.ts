fd0185f6030f69a03fb7c9cfe76d8367cffb24d3
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-10-02 19:45:47 +0530

    fix: FFD

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -2,5 +2,5 @@ export const Socials = {
   GitHub: 'https://github.com/Pujo-Atlas-Kolkata/PujoAtlasKol-Web',
   Discord: 'https://discord.com/invite/xxSXWYf6d4',
   Reddit: 'https://www.reddit.com/r/kolkata',
-  FeedbackForm: 'https://forms.gle/7Q6Q6Q7Q6Q7Q6Q7Q6',
+  FeedbackForm: 'https://forms.gle/AsZwTvmzwR9tptGH6',
 } as const;
