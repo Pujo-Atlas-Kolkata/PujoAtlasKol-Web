3875e1fe8f678c48e4efaa47423753fb86d1455a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-10-02 19:02:33 +0530

    feat: ff

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -2,4 +2,5 @@ export const Socials = {
   GitHub: 'https://github.com/Pujo-Atlas-Kolkata/PujoAtlasKol-Web',
   Discord: 'https://discord.com/invite/xxSXWYf6d4',
   Reddit: 'https://www.reddit.com/r/kolkata',
+  FeedbackForm: 'https://forms.gle/7Q6Q6Q7Q6Q7Q6Q7Q6',
 } as const;
