bca5baaa673bd8121ed779d9a9bf36ba7fba0644
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2024-10-05 16:27:20 +0530

    feat: modifying the settings drawer

--- src/components/app/shared/SettingsFlyout.tsx
+++ src/components/app/shared/SettingsFlyout.tsx
@@ -1,10 +1,11 @@
 import { Drawer } from 'vaul';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, type SetStateAction } from 'react';
 import { Socials } from '@/constants';
 
 export default function SettingsFlyout() {
   const [isOpen, setIsOpen] = useState(false);
   const [container, setContainer] = useState<HTMLDivElement | null>(null);
+  const [locationStatus, setLocationStatus] = useState('off');
 
   useEffect(() => {
     const handleToggleFlyout = () => setIsOpen((prev) => !prev);
@@ -17,6 +18,47 @@ export default function SettingsFlyout() {
       document.removeEventListener('toggleFlyout', handleToggleFlyout);
     };
   }, [isOpen]);
+
+  useEffect(() => {
+    if (navigator.permissions) {
+      navigator.permissions.query({ name: 'geolocation' }).then((permissionStatus) => {
+        updateLocationStatus(permissionStatus.state);
+        permissionStatus.onchange = function () {
+          updateLocationStatus(this.state);
+        };
+      });
+    }
+  }, []);
+
+  const updateLocationStatus = (state: string) => {
+    if (state === 'granted') {
+      setLocationStatus('on');
+    } else {
+      setLocationStatus('off');
+    }
+  };
+
+  const handleToggle = () => {
+    if (locationStatus === 'off') {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log('Location granted:', position.coords);
+          setLocationStatus('on');
+        },
+        (error) => {
+          console.error('Error getting location:', error.message);
+        },
+      );
+    } else {
+      setTimeout(() => {
+        alert('Unable to retrieve your location. Please check location permissions.');
+      }, 5000);
+    }
+  };
+
+  const handleError = (errorMessage: SetStateAction<string | null>) => {
+    alert(errorMessage);
+  };
 
   return (
     <div className="font-sans">
@@ -38,11 +80,11 @@ export default function SettingsFlyout() {
              max-w-[calc(100% - 1rem)]
              shadow-lg"
               >
-                <Drawer.Title className="text-base px-0 py-0">
+                <Drawer.Title className="text-base ">
                   <div className="rounded-full h-[6px] bg-[#4a4a4a] w-9 mb-3 mt-1 mx-auto"></div>
-                  <div className="flex flex-row justify-between items-center px-3 py-0">
-                    <div className="text-lg">Language</div>
-                    <div className="flex items-center bg-inherit rounded-r-full p-1 w-fit text-base">
+                  <div className="flex flex-row  items-center m-2 w-full">
+                    <div className="text-lg w-[60%]  ">Language</div>
+                    <div className="flex items-center bg-inherit rounded-r-full p-1 w-fit text-base  ">
                       <button className="bg-[#c5b394] font-semibold text-white px-3 py-2 text-sm rounded-l-full transition-all">
                         English
                       </button>
@@ -51,7 +93,7 @@ export default function SettingsFlyout() {
                         disabled
                         className="text-black px-3 py-2 font-semibold relative flex flex-col justify-center items-center"
                       >
-                        <span className="font-noto pt-1 text-base">বাংলা</span>
+                        <span className="font-noto text-base">বাংলা</span>
                       </button>
                     </div>
                   </div>
@@ -63,10 +105,34 @@ export default function SettingsFlyout() {
 
                 <hr className="border-gray-950/10 border-[0.5px] mx-2" />
 
-                <div className="flex flex-col items-start justify-center gap-y-0 px-3 py-1.5">
-                  <div className="text-lg">Location Access</div>
-                  <div className="text-xs">
-                    This can be managed in the browser per-site settings
+                <div className="w-full flex justify-between">
+                  <div className="m-2 ">
+                    <div className="text-lg">Location Access</div>
+                    <div className="text-xs my-1">
+                      This can be managed in the browser per-site settings
+                    </div>
+                  </div>
+
+                  <div className="flex items-center bg-inherit rounded-r-full p-1 w-fit text-base">
+                    <button
+                      onClick={handleToggle}
+                      className={`${
+                        locationStatus === 'on' ? 'bg-[#c5b394]' : 'bg-#dddddd'
+                      } font-semibold text-white px-2 py-2 text-sm rounded-l-full transition-all`}
+                    >
+                      {locationStatus === 'on' ? 'Enabled' : 'Disabled'}
+                    </button>
+
+                    <button
+                      className={`${
+                        locationStatus === 'on' ? 'bg-[#dddddd]' : 'bg-[#c5b394]'
+                      } text-black px-2 py-1 font-semibold relative flex flex-col justify-center items-center rounded-r-full`}
+                      onClick={() => handleError('Please enable location settings of the browser')}
+                    >
+                      <span className="font-noto text-center w-full ">
+                        {locationStatus === 'on' ? 'Disable' : 'Enable'}
+                      </span>
+                    </button>
                   </div>
                 </div>
 
