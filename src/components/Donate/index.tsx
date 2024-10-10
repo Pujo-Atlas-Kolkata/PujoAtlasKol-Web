dc5a0ec2581bdaddb9b3361850bf461f8995bec3
Wriddhi Hazra <wriddhihazra@gmail.com>
2024-10-11 01:28:41 +0530

    fix: style fixes + env vpa

--- src/components/Donate/index.tsx
+++ src/components/Donate/index.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/libs/utils';
-import { BsChevronExpand } from 'react-icons/bs';
 import { useMemo, useState } from 'react';
 import QRCode from 'react-qr-code';
+import { Socials } from '@/constants';
 
 type UPIDetails = {
   pa: string;
@@ -19,138 +19,136 @@ const createUPIUrl = (details: UPIDetails) => {
   return `upi://pay?${params.join('')}`;
 };
 
-const definedAmounts = ['50.00', '100.00', '200.00', '500.00', '1000.00'];
-const MIN = '20.00';
+const definedAmounts = ['50', '100', '200', '500', '1000', '2000'];
+const MIN = '20';
+const MAX = '100000';
 
 const isGreaterThanMin = (value: string) => {
-  return parseFloat(value) >= parseFloat(MIN);
+  return parseInt(value) >= parseInt(MIN);
 };
 
-export const Donate = () => {
+const isLessThanMax = (value: string) => {
+  return parseInt(value) <= parseInt(MAX);
+};
+
+type Props = {
+  id: string;
+};
+
+export const Donate = ({ id }: Props) => {
   const [amount, setAmount] = useState(definedAmounts[0]);
-  const [isCustomAmount, setIsCustomAmount] = useState(false);
-  const [openOptions, setOpenOptions] = useState(false);
+  const [isCustomAmount, setCustomAmount] = useState(false);
+
+  const validAmount = useMemo(() => {
+    return amount.length && isGreaterThanMin(amount) ? (isLessThanMax(amount) ? amount : MAX) : MIN;
+  }, [amount]);
 
   const UPI = useMemo(() => {
     const details: UPIDetails = {
-      pa: 'avimanyudas08-1@okicici',
+      pa: id,
       pn: 'Pujo Atlas',
       cu: 'INR',
-      am: amount,
+      am: validAmount,
       tn: 'Donation to Pujo Atlas',
     };
     return createUPIUrl(details);
-  }, [amount]);
+  }, [validAmount, id]);
 
   return (
-    <section id="donation" className="flex flex-col gap-4">
-      <h2 className="font-work font-semibold md-auto md:mr-auto text-2xl lg:text-4xl">
-        Donate via UPI
-      </h2>
-      <QRCode
-        value={UPI}
-        fgColor="#332f28"
-        bgColor="#ffedc9"
-        className="p-2 outline outline-4 mx-auto size-40 lg:size-64"
-      />
-      <div
-        className={cn(
-          'font-medium flex gap-1 items-center relative',
-          'w-40 lg:w-64 mx-auto p-2 rounded-lg',
-          'outline outline-primary-foreground',
-          'shadow-[2px_2px_0_2px] shadow-primary-foreground',
-        )}
-        role="combobox"
-      >
-        &#8377;
-        <div className="w-fit max-w-20 lg:max-w-32 bg-transparent mr-auto">
-          {isCustomAmount ? (
-            <input
-              type="numeric"
-              autoFocus
-              value={amount}
-              onBlur={() => {
-                if (amount.trim().length === 0 || !isGreaterThanMin(amount)) {
-                  setAmount(MIN);
-                }
-              }}
-              onChange={(e) => {
-                const isValidNumber = /^[0-9]*\.?[0-9]*$/.test(e.target.value);
-                if (!isValidNumber) return;
-                setAmount(e.target.value);
-              }}
-              className="w-full bg-transparent outline-none placeholder-primary-foreground"
-              placeholder={'20.00'}
-            />
-          ) : (
-            <span>{amount}</span>
-          )}
-        </div>
-        <ul
-          data-active={openOptions}
-          className={cn(
-            'h-0 data-[active=true]:h-auto overflow-hidden mb-4',
-            'absolute top-full translate-y-2 left-0 w-full',
-            'bg-primary-background rounded-lg data-[active=true]:outline',
-            'data-[active=true]:shadow-[2px_2px_0_2px] shadow-black',
-          )}
-        >
-          {definedAmounts.map((amt) => (
-            <li
-              key={amt}
-              role="option"
-              data-selected={amt === amount}
-              className={cn(
-                'cursor-pointer p-2 flex gap-1 group',
-                'data-[selected=true]:bg-primary-foreground data-[selected=true]:!text-white',
-              )}
-              onClick={() => {
-                setAmount(amt);
-                setIsCustomAmount(false);
-                setOpenOptions(false);
-              }}
-            >
-              &#8377; <span className="group-data-[selected=true]:!text-white">{amt}</span>
-            </li>
-          ))}
-          <li
-            role="option"
-            data-selected={isCustomAmount && !definedAmounts.includes(amount)}
-            className={cn(
-              'cursor-pointer p-2 flex gap-1 group',
-              'data-[selected=true]:bg-primary-foreground data-[selected=true]:!text-white',
-            )}
-            onClick={() => {
-              setAmount(definedAmounts[0]);
-              setIsCustomAmount(true);
-              setOpenOptions(false);
-            }}
-          >
-            Custom Amount
-          </li>
-        </ul>
-        <BsChevronExpand
-          role="button"
-          onClick={() => {
-            setOpenOptions(!openOptions);
-          }}
+    <section id="donation" className="flex flex-col items-center lg:items-start gap-12">
+      <h2 className="font-work font-semibold w-fit text-2xl lg:text-4xl">Donate via UPI</h2>
+      <div className="flex flex-col items-center justify-evenly lg:flex-row gap-4 lg:gap-12">
+        <QRCode
+          value={UPI}
+          fgColor="#332f28"
+          bgColor="#ffedc9"
+          level="L"
+          className="p-1 outline outline-4 w-fit rounded-xl size-64 lg:size-64"
         />
+        <div className="flex flex-col gap-4 lg:h-full">
+          <div className="w-64 mx-auto grid grid-cols-3 gap-4">
+            {definedAmounts.map((amt) => (
+              <button
+                key={amt}
+                data-active={amt === validAmount}
+                className={cn(
+                  'font-medium flex gap-1 items-center relative',
+                  'p-2 rounded-lg data-[active=true]:bg-secondary-background',
+                  'outline outline-primary-foreground',
+                  'shadow-[2px_2px_0_2px] shadow-primary-foreground',
+                )}
+                onClick={() => {
+                  setAmount(amt);
+                  setCustomAmount(false);
+                }}
+              >
+                &#8377; {amt}
+              </button>
+            ))}
+            <button
+              data-active={isCustomAmount}
+              className={cn(
+                'font-medium flex gap-1 items-center relative',
+                'p-2 rounded-lg data-[active=true]:bg-secondary-background',
+                'outline outline-primary-foreground',
+                'shadow-[2px_2px_0_2px] shadow-primary-foreground',
+              )}
+              onClick={() => setCustomAmount((prev) => !prev)}
+            >
+              Custom
+            </button>
+            {isCustomAmount && (
+              <input
+                type="number"
+                min={MIN}
+                max={MAX}
+                value={amount}
+                placeholder={MIN}
+                onChange={(e) => {
+                  // regex to only allow numbers without decimal or negative sign
+                  const isNumber = /^[0-9]+$/.test(e.target.value);
+                  console.log('Validity = ', isNumber);
+                  if (isNumber) {
+                    setAmount(e.target.value);
+                  }
+                }}
+                onBlur={() => setAmount(validAmount)}
+                className={cn(
+                  'p-2 rounded-lg col-span-2 text-left font-semibold placeholder-primary-foreground',
+                  'outline outline-primary-foreground shadow-[2px_2px_0_2px] shadow-primary-foreground',
+                )}
+              />
+            )}
+          </div>
+          <a
+            className={cn(
+              'md:hidden p-2 w-full mx-auto text-center rounded-lg',
+              'font-work bg-black !text-white font-medium outline outline-4 outline-black',
+            )}
+            href={UPI}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Donate &#8377; {validAmount}
+          </a>
+          <span className="text-left font-work font-medium hidden md:inline mt-auto">
+            Scan the QR to donate &#8377; {validAmount}
+          </span>
+        </div>
       </div>
-      <a
-        className={cn(
-          'md:hidden p-2 w-40 mx-auto text-center rounded-lg',
-          'font-work bg-black !text-white font-medium outline outline-4 outline-black',
-        )}
-        href={UPI}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Donate &#8377; {amount}
-      </a>
-      <span className="text-center font-work font-medium hidden md:inline">
-        Scan the QR to donate &#8377;
-        {amount.length && isGreaterThanMin(amount) ? amount : MIN}
-      </span>
+      <p className="text-justify">
+        More donation options will be available soon! For any doubts or queries regarding donating
+        to Pujo Atlas, please enquire in our{' '}
+        <a
+          href={Socials.Discord}
+          target="_blank"
+          rel="noreferrer"
+          className="font-semibold font-work border-b-2 border-dashed border-black"
+        >
+          Discord Server
+        </a>
+        .
+      </p>
     </section>
   );
 };
