66426f5bd9bbbff11eb40cfc7f814797a5f10409
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2025-05-30 03:10:48 +0530

    feat: integrate Sentry for error tracking and monitoring
    - Added Sentry configuration for both server and edge environments to capture errors and performance metrics.
    - Implemented client-side error handling with a GlobalError component to report exceptions to Sentry.
    - Created example API and frontend pages to demonstrate Sentry's error tracking capabilities.
    - Updated package dependencies to include Sentry for Next.js, enhancing the application's observability and debugging capabilities.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 import "./src/env.js";
+import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -31,4 +32,29 @@ const config = {
   skipTrailingSlashRedirect: true,
 };
 
-export default config;
+const sentryOptions = {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+  org: "pujoatlas-us",
+  project: "atlas-nextjs-v2",
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+};
+
+export default withSentryConfig(config, sentryOptions);
