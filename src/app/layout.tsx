3f682ba38015e58cce88db2321d95571ec7effc5
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2025-05-30 19:16:44 +0530

    fix: update metadataBase logic for environment detection
    - Changed the environment variable from NODE_ENV to ENVIRONMENT for determining the metadataBase URLs in layout.tsx, ensuring correct URL assignment for production and development environments.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,11 @@ import { PostHogProvider } from "@/components/PostHogProvider";
 
 export const metadata: Metadata = {
   metadataBase:
-    process.env.NODE_ENV === "production"
+    process.env.ENVIRONMENT === "production"
       ? new URL("https://atlas.ourkolkata.in")
-      : new URL("https://deploy-preview-194.pr.pujo-atlas.ourkolkata.in"),
+      : process.env.ENVIRONMENT === "development"
+        ? new URL("https://deploy-preview-194.pr.pujo-atlas.ourkolkata.in")
+        : new URL("https://deploy-preview-194.pr.pujo-atlas.ourkolkata.in"),
   title: "Pujo Atlas - Interactive Maps and Real-Time Updates for Durga Puja",
   description:
     "Discover the best Pandals with Pujo Atlas! Real-time updates, interactive maps and emergency helplines, for the ultimate Durga Puja Porikormo experience in Kolkata.",
