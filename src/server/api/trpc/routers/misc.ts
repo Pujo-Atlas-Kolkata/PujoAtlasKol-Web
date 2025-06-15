37965f8c3bf500eed9bfbdd6ccdb3d4aec279fca
Sayantan Basu <sayantanbasu@live.in>
2025-06-15 23:28:28 +0530

    add auth middleware for JWT
    - PA-126

--- src/server/api/trpc/routers/misc.ts
+++ src/server/api/trpc/routers/misc.ts
@@ -1,8 +1,8 @@
-import { baseProcedure, createTRPCRouter } from "@/server/api/trpc/init";
+import { protectedProcedure, createTRPCRouter } from "@/server/api/trpc/init";
 import { checkServerHealth } from "@/server/api/functions";
 
 export const miscRouter = createTRPCRouter({
-  healthCheckPing: baseProcedure.query(() => {
+  healthCheckPing: protectedProcedure.query(() => {
     return checkServerHealth();
   }),
 });
