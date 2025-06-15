8c88e82b02edf3de76e7188c93f1121a61ae8a45
Andrew99xx <indranilchakraborty4sc@gmail.com>
2025-06-15 18:35:03 +0530

    feat(db): Added Drizzle DB ORM Client

--- src/server/api/trpc/routers/misc.ts
+++ src/server/api/trpc/routers/misc.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "@/server/api/trpc/init";
 import { checkServerHealth } from "@/server/api/functions";
 
@@ -5,4 +6,10 @@ export const miscRouter = createTRPCRouter({
   healthCheckPing: baseProcedure.query(() => {
     return checkServerHealth();
   }),
+
+  hello: baseProcedure
+    .input(z.object({ name: z.string().optional() }))
+    .query(({ input }) => {
+      return { message: `Hello ${input.name ?? "World"}!` };
+    }),
 });
