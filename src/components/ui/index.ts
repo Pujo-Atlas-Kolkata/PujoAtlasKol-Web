a363d747d62bdd690e0666fa45e34227784f8db3
Soumyadeep <umyadeepb27@gmail.com>
2025-07-03 09:15:32 +0530

    chore: marquee component removed

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,6 @@
 export * from "@/components/ui/button";
 export * from "@/components/ui/badge";
 export * from "@/components/ui/evervault-card";
-export * from "@/components/ui/marquee";
 export * from "@/components/ui/sheet";
 export * from "@/components/ui/sonner";
 export * from "@/components/ui/tooltip";
