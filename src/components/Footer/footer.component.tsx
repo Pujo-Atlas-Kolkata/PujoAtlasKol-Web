f46a1945a175dd21ad3db6eafe63afe400ecd239
Soumyadeep <umyadeepb27@gmail.com>
2025-07-04 13:40:06 +0530

    fix: footer UI mobile

--- src/components/Footer/footer.component.tsx
+++ src/components/Footer/footer.component.tsx
@@ -2,16 +2,25 @@
 
 import { Constants } from "@/lib";
 import { ArrowUpRight } from "lucide-react";
+import { FaDiscord, FaGithub, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import Link from "next/link";
 import posthog from "posthog-js";
 import { PostHogEvents } from "@/providers";
 import { Button } from "@/components";
 
 export const Footer = () => {
+  const socialIcons = {
+    discord: FaDiscord,
+    gitHub: FaGithub,
+    facebook: FaFacebook,
+    instagram: FaInstagram,
+    twitter: FaTwitter,
+  };
+
   return (
     <footer className="flex-w mx-auto my-3 flex w-[calc(100%-1rem)] flex-col items-center rounded-lg border-2 border-black bg-amber-300/30 p-2 backdrop-blur-sm lg:my-10 lg:w-[calc(100%-5rem)] lg:p-12">
       {/* Footer for large screens */}
-      <div className="block w-full">
+      <div className="hidden w-full lg:block">
         <div className="lg:items:center flex w-full flex-col items-start justify-between gap-3 lg:flex-row">
           <div className="text-normal flex flex-row flex-wrap items-center gap-2 text-center font-normal lg:text-lg">
             <div>
@@ -149,105 +158,79 @@ export const Footer = () => {
       </div>
 
       {/* Footer for mobile screens */}
-      <div className="hidden w-full text-center">
-        <div className="flex flex-col items-start justify-start text-sm font-normal">
-          <div>
-            Cooked with ❤️ by&nbsp;
-            <span className="font-medium">Pujo Atlas Kolkata</span>.
+      <div className="block w-full lg:hidden">
+        <div className="mt-5 flex w-full flex-col items-center justify-between gap-3">
+          <div className="text-normal flex flex-col flex-wrap items-center gap-2 text-center font-normal">
+            <div>
+              Cooked with ❤️ by&nbsp;
+              <span className="font-medium">Pujo Atlas Kolkata</span>.
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              Checkout&nbsp;
+              <span className="font-medium">Atlas v1 Wrapped</span>
+              &nbsp;2024&nbsp;
+              <a
+                href={Constants.stats.redditPost}
+                className="font-semibold underline"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={() =>
+                  posthog?.capture(PostHogEvents.FOOTER_CLICK, {
+                    link: "redditPost",
+                  })
+                }
+              >
+                <div className="flex flex-row items-center justify-center">
+                  here
+                  <ArrowUpRight className="size-4 animate-pulse" />
+                </div>
+              </a>
+            </div>
           </div>
 
-          <div className="flex">
-            Checkout&nbsp;
-            <span className="font-medium">Atlas v1 Wrapped</span>
-            &nbsp;2024&nbsp;
-            <a
-              href={Constants.stats.redditPost}
-              className="font-semibold underline"
+          <div className="flex flex-row flex-wrap items-center justify-start gap-3 text-sm underline">
+            <Link
+              href={Constants.socials.discord}
               target="_blank"
               rel="noopener noreferrer"
               onClick={() =>
                 posthog?.capture(PostHogEvents.FOOTER_CLICK, {
-                  link: "redditPost",
+                  link: "discord",
                 })
               }
             >
-              <div className="flex flex-row items-center justify-center">
-                here
-                <ArrowUpRight className="mb-0.5 size-3 animate-pulse" />
-              </div>
-            </a>
+              Bug?
+            </Link>
+
+            <Link
+              href={Constants.misc.feedbackForm}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() =>
+                posthog?.capture(PostHogEvents.FOOTER_CLICK, {
+                  link: "feedbackForm",
+                })
+              }
+            >
+              Feedback
+            </Link>
+
+            <Link
+              href={Constants.misc.statusPage}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() =>
+                posthog?.capture(PostHogEvents.FOOTER_CLICK, {
+                  link: "statusPage",
+                })
+              }
+            >
+              Status
+            </Link>
           </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-3 pt-2 text-xs font-medium">
-          <Link
-            href={Constants.socials.discord}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={() =>
-              posthog?.capture(PostHogEvents.FOOTER_CLICK, {
-                link: "discord",
-              })
-            }
-            className="rounded-sm bg-black/45 px-3 pt-2.5 pb-2 font-medium text-white"
-          >
-            Bug?
-          </Link>
-
-          <Link
-            href={Constants.misc.feedbackForm}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={() =>
-              posthog?.capture(PostHogEvents.FOOTER_CLICK, {
-                link: "feedbackForm",
-              })
-            }
-            className="rounded-sm bg-black/45 px-3 pt-2.5 pb-2 font-medium text-white"
-          >
-            Feedback
-          </Link>
-
-          <Link
-            href={Constants.misc.statusPage}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={() =>
-              posthog?.capture(PostHogEvents.FOOTER_CLICK, {
-                link: "statusPage",
-              })
-            }
-            className="rounded-sm bg-black/45 px-3 pt-2.5 pb-2 font-medium text-white"
-          >
-            Status
-          </Link>
-
-          <Link
-            href={Constants.socials.gitHub}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={() => posthog?.capture(PostHogEvents.FOOTER_CLICK, { link: "github" })}
-            className="rounded-sm bg-black/45 px-3 pt-2.5 pb-2 font-medium text-white"
-          >
-            Contribute
-          </Link>
-
-          <Link
-            href={Constants.routes.sponsorUs}
-            onClick={() =>
-              posthog?.capture(PostHogEvents.FOOTER_CLICK, {
-                link: "sponsor-us",
-              })
-            }
-            className="rounded-sm bg-black/45 px-3 pt-2.5 pb-2 font-medium text-white"
-          >
-            Sponsor Us
-          </Link>
-        </div>
-
-        <div className="grid grid-cols-3 gap-3 pt-3.5 text-xs font-medium">
-          <p className="col-span-3 -mb-2 text-left text-sm font-medium">Find us on</p>
-
+        <div className="text-large mt-5 flex flex-row items-center justify-center gap-3">
           {Object.entries(Constants.socials)
             .filter(([platform]) => platform !== "sponsor")
             .map(([platform, url]) => (
@@ -261,29 +244,40 @@ export const Footer = () => {
                     link: platform,
                   })
                 }
-                className="rounded-sm bg-black/45 px-3 pt-2.5 pb-2 font-medium text-white"
               >
-                {platform.charAt(0).toUpperCase() + platform.slice(1)}
+                {socialIcons[platform as keyof typeof socialIcons]
+                  ? (() => {
+                      const Icon = socialIcons[platform as keyof typeof socialIcons];
+                      return <Icon size={24} className="inline-block" />;
+                    })()
+                  : platform.charAt(0).toUpperCase() + platform.slice(1)}
               </Link>
             ))}
         </div>
 
-        <div className="flex flex-row items-center justify-center pt-6 text-xs">
-          <div>Copyright © 2025 Pujo Atlas Kolkata.&nbsp;</div>
+        <div className="mt-5 mb-4 h-0.5 w-full bg-black" />
 
-          <Link
-            href={Constants.misc.license}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-semibold hover:underline"
-            onClick={() =>
-              posthog?.capture(PostHogEvents.FOOTER_CLICK, {
-                link: "license",
-              })
-            }
-          >
-            MIT Licensed.
-          </Link>
+        <div className="flex w-full flex-col flex-wrap items-center justify-between gap-3">
+          <div className="flex flex-wrap gap-2 text-sm font-normal">
+            <div>Copyright © 2025 Pujo Atlas Kolkata.</div>
+            <Link
+              href={Constants.misc.license}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-semibold"
+              onClick={() =>
+                posthog?.capture(PostHogEvents.FOOTER_CLICK, {
+                  link: "license",
+                })
+              }
+            >
+              <div className="flex flex-row items-center justify-center">
+                MIT&nbsp;
+                <span className="underline">License</span>
+                <ArrowUpRight className="size-4 animate-pulse" />
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
