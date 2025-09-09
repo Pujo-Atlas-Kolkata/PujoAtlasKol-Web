a4e13a085eae53ab4044b6c59343990b3f662c88
Soumyadeep <umyadeepb27@gmail.com>
2025-09-09 11:33:01 +0530

    fix: format fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/server/db/schema.ts',
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
